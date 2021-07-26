b9ae29cbc9060d8db4ac91d28802a5384a28acf6
EC2 Default User <ec2-user@ip-172-31-4-42.ec2.internal>
2021-07-26 16:33:34 +0000

    完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
@@ -22,6 +23,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+    redirect_to books_path
+    end
   end
 
 
@@ -35,9 +39,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
